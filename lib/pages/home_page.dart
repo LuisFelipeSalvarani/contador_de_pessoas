bbee250cd6d5df7a23f034de7802ff640257631b
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-05-08 20:23:44 -0300

    Final do app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+// import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -30,55 +30,64 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                contador >= 15 ? "Lotado" : "Pode entrar",
-                style: TextStyle(fontSize: 40),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                contador.toString(),
-                style: TextStyle(fontSize: 80),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-                  child: ElevatedButton(
-                  onPressed: incrementar,
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(100, 100),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                    backgroundColor: contador >=15 ? Colors.red : Colors.grey
-                    ),
-                  child: Text("Entrou"),
-                ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: 
+            DecorationImage(
+              image: AssetImage("assets/images/liquid-cheese.png"),
+              fit: BoxFit.cover
               ),
-              ElevatedButton(
-                onPressed: decrementar,
-                style: ElevatedButton.styleFrom(
-                  fixedSize: Size(100, 100),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                  backgroundColor: contador <= 0 ? Colors.red : Colors.grey
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  contador >= 15 ? "Lotado" : "Pode entrar",
+                  style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  contador.toString(),
+                  style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold, color: contador >=15 ? Colors.red : Colors.black),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    child: ElevatedButton(
+                    onPressed: incrementar,
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 100),
+                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                      backgroundColor: contador >=15 ? Colors.white.withOpacity(0.2) : Colors.white
+                    ),
+                    child: const Text("Entrou", style: TextStyle(color: Colors.black)),
                   ),
-                child: Text("Saiu"),
-              )
-            ],
-          ),
-        ],
-      ),
+                ),
+                ElevatedButton(
+                  onPressed: decrementar,
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(100, 100),
+                    shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                    backgroundColor: contador <= 0 ? Colors.white.withOpacity(0.2) : Colors.white
+                  ),
+                  child: const Text("Saiu", style: TextStyle(color: Colors.black)),
+                )
+              ],
+            ),
+          ],
+        ),
+      )
     );
   }
 }
